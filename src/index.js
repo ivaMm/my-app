9b8c8671100428ad8e57f70bc776c231dcce61e0
iva mudronja <iva.pilizota@gmail.com>
2021-04-27 17:50:19 +0200

    added form and list

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+/*
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -15,3 +16,46 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
+
+function AddPersonForm() {
+  const [person, setPerson ] = useState
+  ("");
+
+  function handleChange(e) {
+    setPerson(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDeafault();
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text"
+        placeholder="Add new contact"
+        onChange={handleChange}
+        value={person.name} />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
+
+function PeopleList(props) {
+  const arr = props.data;
+  const listItems = arr.map((val, index) =>
+    <li key={index}>{val}</li>
+  );
+  return <ul>{listItems}</ul>;
+}
+
+const contacts = ["James Smith", "Thomas Anderson", "Bruce Wayne"];
+const el = (
+  <div>
+    <AddPersonForm />
+    <PeopleList data={contacts} />
+  </div>
+);
+ReactDOM.render(
+  el,
+  document.getElementById('root')
+)
